47e902d9668d44dca81ae6c582689734911716ad
Karnsinh <karnsinh96@gmail.com>
2023-08-08 11:35:26 +0530

    Prediction Pipeline Completed

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 import os 
 import sys
 import dill
+import pickle
 
 import pandas as pd
 import numpy as np
@@ -36,6 +37,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
             report[list(models.keys())[i]] = test_model_score
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
