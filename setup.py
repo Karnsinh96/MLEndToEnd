829e411697ec08cd5e318cfdc37b9d3882fa2a98
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-01 16:29:26 +0530

    Changes On 1 Jan 2024

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='MLEndToEnd',
 version='0.0.1',
 author='Karnsinh',
 author_email='karnsinh96@gmail.com',
